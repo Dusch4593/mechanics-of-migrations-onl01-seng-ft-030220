cb48b5383b1bd38abb71ed8dfbeda286722f694d
Brandon Dusch <brandondusch@gmail.com>
2020-04-08 16:51:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change # common for basic migrations
-    drop_table :artists
     create_table :artists do |column|
       column.string :name
       column.string :genre
